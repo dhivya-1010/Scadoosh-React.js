10f7c88078f2ccae8722c849c00267df7b1ad543
dhivya-1010 <dhivya.v2024cse@sece.ac.in>
2026-06-09 12:41:02 +0530

    change count using arrow functions

--- reactApp/src/assets/jsxconcept/change.jsx
+++ reactApp/src/assets/jsxconcept/change.jsx
@@ -3,18 +3,18 @@ import { useState } from "react";
 export default function changeApp(){
     const[change,setChange]=useState(0);
 
-    function onInc(){
-        setChange(change+1);
-    }
-    function onDec(){
-        setChange(change-1);
-    }
+    // function onInc(){
+    //     setChange(change+1);
+    // }
+    // function onDec(){
+    //     setChange(change-1);
+    // }
 
     return(
         <div>
             <h1>count: {change}</h1>
-            <button onClick={onInc}>+</button>
-            <button onClick={onDec}>-</button>
+            <button onClick={()=>{setChange(change+1)}}>+</button>
+            <button onClick={()=>{setChange(change-1)}}>-</button>
         </div>
     );
 }
