9c976c53a67bec8146a5f09d623764d6b1be2ecd
dhivya-1010 <dhivya.v2024cse@sece.ac.in>
2026-06-09 15:11:01 +0530

    event-target-value

--- reactApp/src/assets/eventHandling/eventtarget.jsx
+++ reactApp/src/assets/eventHandling/eventtarget.jsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
 
 export default function eventtarget(){
+    const[text,setText]=useState("");
     function handlechange(){
         console.log(event.target);
         console.log(event.target.value);
+        setText(event.target.value)
     }
     return(
         <div>
+            <h2>Event-target-value</h2>
+            <h2>{text}</h2>
             <input type="text" placeholder="enter text" onChange={handlechange}></input>
+            
         </div>
     );
 }
