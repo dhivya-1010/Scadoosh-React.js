e0effde1440da8215a98f065ebb315b3313eb95d
dhivya-1010 <dhivya.v2024cse@sece.ac.in>
2026-06-10 15:53:27 +0530

    useRef

--- reactApp/src/assets/hooks/userefexample.jsx
+++ reactApp/src/assets/hooks/userefexample.jsx
@@ -1,19 +1,39 @@
-import { useState,useEffect } from "react";
+// import { useState,useEffect } from "react";
+
+// export default function usesrefexample(){
+//     const[count,setCount] = useState(0);
+    
+//     function handleClick(){
+//         setCount(count+1);
+//     }
+//     useEffect(()=>{
+//         console.log(count)
+//     },[count]);
+    
+//     return(
+//         <div>
+//             <h1 style={ {fontFamily:"cursive",color:"brown" } } >UseRef Example</h1>
+//             <h2>Number: {count}</h2>
+//             <button onClick={handleClick} > Click </button>
+//         </div>
+//     );
+// }
+
+import { useRef } from "react";
 
 export default function usesrefexample(){
-    const[count,setCount] = useState(0);
+    const myref = useRef(0);
+    console.log("component runs");
     
     function handleClick(){
-        setCount(count+1);
-    }
-    useEffect(()=>{
-        console.log(count)
-    },[count]);
+        myref.current+=1;
+        console.log(myref.current)   
+    };
     
     return(
         <div>
             <h1 style={ {fontFamily:"cursive",color:"brown" } } >UseRef Example</h1>
-            <h2>Number: {count}</h2>
+            <h2>Number: {myref.current}</h2>
             <button onClick={handleClick} > Click </button>
         </div>
     );
